625923156c3e520f2d4791af8cd731c59862803d
Amritpaul Sidhu <asidhu2001@csu.fullerton.edu>
2022-09-17 17:46:57 -0700

    added endpoint functions

---
+++ endpoint_functions.py
@@ -0,0 +1,49 @@
+import string
+import os
+import requests
+
+"""Endpoint Functions"""
+
+payload = {'api_key': os.getenv('API_KEY')}
+
+
+def summonerID(id):
+    encryptedSummonerId = id
+    endpoint = 'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{}'.format(encryptedSummonerId)
+    json = requests.get(endpoint, params=payload)
+    return json
+
+def summonerName(name):
+    summoner_name = name
+    endpoint = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}'.format(summoner_name)
+    json = requests.get(endpoint, params=payload)
+    return json
+
+def matchID(id):
+    match_id = id
+    endpoint = 'https://americas.api.riotgames.com/lol/match/v5/matches/{}'.format(match_id)
+    json = requests.get(endpoint, params=payload)
+    return json
+
+# /lol/match/v5/matches/by-puuid/{puuid}/ids
+
+def matchList(id):
+    puuid = id
+    endpoint = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids'.format(puuid)
+    json = requests.get(endpoint, params=payload)
+    return json
+    
+    
+"""tests"""
+
+result = summonerID("GtuLgsQ0rBzcKnjEagd6vZhT3YMvKAwW5Lwy1khpK9eUWYw")
+print(result.json())
+
+result2 = summonerName('Dollheart')
+print(result2.json())
+
+result3 = matchList('tVek217N2bNcy-YFK2uIuXSe4zM1UEq1YhnDApul37etd3rImE3a85WnewqoEdZZUpGxzNJxpXeBsA')
+print(result3.json())
+
+result4 = matchID('NA1_4436996610')
+print(result4.json())
